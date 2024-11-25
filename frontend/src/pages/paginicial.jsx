0f8be0cd91966f0faf98f4cdd393f1d7d95ded7a
RyanSysten02 <redepublica1a8@hotmail.com>
2024-11-24 23:15:34 -0300

    ajuste de telas

--- frontend/src/pages/paginicial.jsx
+++ frontend/src/pages/paginicial.jsx
@@ -1,34 +1,16 @@
-import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
-import Cabecalho from "../componentes/Cabecalho";
-import { Container } from "reactstrap";
+import Adicionar from "../componentes/agenda/adicionar/Adicionar";
+import FormAdicionar from "../componentes/agenda/adicionar/formadicionar";
 import Calendario from "../componentes/Calendario";
-import Paciente from "../componentes/paciente/Paciente";
-import Funcionario from "../componentes/funcionario/Funcionario";
+import Pagina from "./PagPadrao";
+
 
 const FullLayout = () => {
   return (
-    <main>
-      {/********header**********/}
-      <Cabecalho />
-      <div className="pageWrapper d-lg-flex">
-        {/********Sidebar**********/}
-        <aside className="sidebarArea shadow" id="sidebarArea">
-          <Sidebar />
-        </aside>
-        {/********Content Area**********/}
-        <div className="contentArea">
-          {/********Middle Content**********/}
-          <Container className="p-4" fluid>
-          
-            <Outlet />
-            <Calendario/>
-            <Paciente/>
-            <Funcionario/>
-          </Container>
-        </div>
-      </div>
-    </main>
+    <Pagina>
+      <Adicionar/>
+      <FormAdicionar/>
+      <Calendario/>
+    </Pagina>
   );
 };
 
--- frontend/src/pages/sidbar2.jsx
+++ frontend/src/pages/sidbar2.jsx
@@ -4,16 +4,15 @@ import {jwtDecode} from 'jwt-decode'
 import { useState, useEffect } from "react";
 import user1 from "../componentes/assets/images/users/user4.jpg";
 import probg from "../componentes/assets/images/bg/download.jpg";
+import './sibar2.css';
+
 
 const navigation = [
-  { title: "Dashboard", href: "/starter", icon: "bi bi-speedometer2" },
-  { title: "Alert", href: "/alerts", icon: "bi bi-bell" },
-  { title: "Badges", href: "/badges", icon: "bi bi-patch-check" },
-  { title: "Buttons", href: "/buttons", icon: "bi bi-hdd-stack" },
-  { title: "Cards", href: "/cards", icon: "bi bi-card-text" },
-  { title: "Forms", href: "/forms", icon: "bi bi-textarea-resize" },
-  { title: "Breadcrumbs", href: "/breadcrumbs", icon: "bi bi-link" },
-  { title: "About", href: "/about", icon: "bi bi-people" },
+  { title: "Agenda", href: "/alerts", icon: "bi bi-calendar-week" },
+  { title: "Agendar consultas", href: "/starter", icon: "bi bi-card-text" },
+  { title: "Pacientes", href: "/alerts", icon: "bi bi-people" },
+  { title: "Medicos", href: "/alerts", icon: "bi bi-person-square" },
+  { title: "Funcionarios", href: "/alerts", icon: "bi bi-person-badge" },
 ];
 
 const Sidebar = () => {
