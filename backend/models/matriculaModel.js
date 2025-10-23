ad37835e8d92aa43f8f19beb095a51f095dd068c
EdersonMendonca <163139960+EdersonMendonca@users.noreply.github.com>
2025-10-23 00:22:40 -0300

    chore: end new page

--- backend/models/matriculaModel.js
+++ backend/models/matriculaModel.js
@@ -38,20 +38,38 @@ const getMatriculas = async () => {
 };
 
 const getMatriculasQuery = async (query) => {
-  const values = query?.periodo?.split(';');
+  const periodo = query?.periodo?.split(';');
+  const turmaId = query?.turma_id;
 
-  const result = await knex
+  let querySelect = knex
     .select(
       'matricula.*',
       'aluno_turma.id as aluno_turma_id',
+      'aluno_turma.campo_unico as campo_unico',
+      'aluno_turma.ano_letivo as ano_letivo',
+      'aluno_turma.turma_id as turma_id',
       'aluno.nome as aluno_nome',
       'turma.nome as turma_nome'
     )
     .from('matricula')
     .innerJoin('aluno', 'matricula.aluno_id', 'aluno.id')
-    .leftJoin('turma', 'matricula.turma_id', 'turma.id')
     .leftJoin('aluno_turma', 'matricula.id', 'aluno_turma.matricula_id')
-    .whereIn('matricula.ano_letivo', values);
+    .leftJoin('turma', 'aluno_turma.turma_id', 'turma.id')
+    .whereNotIn('matricula.status', ['inativa', 'cancelada']);
+
+  if (periodo && periodo.length > 0) {
+    querySelect = querySelect.whereIn('matricula.ano_letivo', periodo);
+  }
+
+  if (turmaId && turmaId !== 'null') {
+    querySelect = querySelect.where('matricula.turma_id', turmaId);
+  }
+
+  if (turmaId === 'null') {
+    querySelect = querySelect.whereNull('matricula.turma_id');
+  }
+
+  const result = await querySelect;
 
   return result;
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,170 +7,173 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import ProtectedRoute from './componentes/protect';
 import Registro from './componentes/register';
-import PagLogin from './pages/login';
-import { LayoutDisciplina } from './pages/pagDisciplina';
-import Layoutresponsavel from './pages/pagResponsavel';
-import { LayoutTurma } from './pages/pagTurma';
-import { LayoutRegistroFrequencia } from './pages/pagRegistroFrequencia';
-import { LayoutConsultaFrequencia } from './pages/pagConsultaFrequencia';
-import Layoutaluno from './pages/pagaluno';
-import FullLayout from './pages/paginicial';
-import Layoutprofessor from './pages/pagprofessor';
-import { LayoutMatricula } from './pages/pagmatricula';
+import LayoutAulas from './pages/Aulas';
 import ConfiguracoesDeSeguranca from './pages/ConfiguracoesDeSeguranca';
 import AcessoNegado from './pages/acessoNegado';
-import { LayoutSubstituicoes } from './pages/pagSubstituicoes';
-import Layoutnotifica from './pages/pagnotifica';
+import PagLogin from './pages/login';
 import LayoutAtendimento from './pages/pagAtendimento';
-import LayoutAulas from './pages/Aulas';
+import { LayoutConsultaFrequencia } from './pages/pagConsultaFrequencia';
+import { LayoutDisciplina } from './pages/pagDisciplina';
 import { LayoutMontarTurma } from './pages/pagMontarTurma';
+import { LayoutRegistroFrequencia } from './pages/pagRegistroFrequencia';
+import Layoutresponsavel from './pages/pagResponsavel';
+import { LayoutSubstituicoes } from './pages/pagSubstituicoes';
+import { LayoutTurma } from './pages/pagTurma';
+import Layoutaluno from './pages/pagaluno';
+import FullLayout from './pages/paginicial';
+import { LayoutMatricula } from './pages/pagmatricula';
+import Layoutnotifica from './pages/pagnotifica';
+import Layoutprofessor from './pages/pagprofessor';
+import { LoadingProvider } from './providers/loading.provider';
 
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<PagLogin />} />
-          <Route path='/login' element={<PagLogin />} />
-          <Route path='/registro' element={<Registro />} />
+      <LoadingProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<PagLogin />} />
+            <Route path='/login' element={<PagLogin />} />
+            <Route path='/registro' element={<Registro />} />
 
-          <Route
-            path='/paginicial'
-            element={
-              <ProtectedRoute>
-                <FullLayout />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path='/paginicial'
+              element={
+                <ProtectedRoute>
+                  <FullLayout />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path='/pagAluno'
-            element={
-              <ProtectedRoute allowedPermissions={['aluno']}>
-                <Layoutaluno />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path='/pagAluno'
+              element={
+                <ProtectedRoute allowedPermissions={['aluno']}>
+                  <Layoutaluno />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path='/pagAulas'
-            element={
-              <ProtectedRoute allowedPermissions={['aluno']}>
-                <LayoutAulas/>
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path='/pagAulas'
+              element={
+                <ProtectedRoute allowedPermissions={['aluno']}>
+                  <LayoutAulas />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path='/pagResponsavel'
-            element={
-              <ProtectedRoute allowedPermissions={['responsavel']}>
-                <Layoutresponsavel />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path='/pagResponsavel'
+              element={
+                <ProtectedRoute allowedPermissions={['responsavel']}>
+                  <Layoutresponsavel />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path='/notifica'
-            element={
-              <ProtectedRoute allowedPermissions={['notifica']}>
-                <Layoutnotifica />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/atendimento'
-            element={
-              <ProtectedRoute allowedPermissions={['notifica']}>
-                <LayoutAtendimento />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path='/notifica'
+              element={
+                <ProtectedRoute allowedPermissions={['notifica']}>
+                  <Layoutnotifica />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/atendimento'
+              element={
+                <ProtectedRoute allowedPermissions={['notifica']}>
+                  <LayoutAtendimento />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path='/pagProfessor'
-            element={
-              <ProtectedRoute allowedPermissions={['professor']}>
-                <Layoutprofessor />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path='/pagProfessor'
+              element={
+                <ProtectedRoute allowedPermissions={['professor']}>
+                  <Layoutprofessor />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path='/pagMatricula'
-            element={
-              <ProtectedRoute allowedPermissions={['matricula']}>
-                <LayoutMatricula />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path='/pagMatricula'
+              element={
+                <ProtectedRoute allowedPermissions={['matricula']}>
+                  <LayoutMatricula />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path='/pagMontarTurma'
-            element={
-              <ProtectedRoute allowedPermissions={['montarturma']}>
-                <LayoutMontarTurma />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path='/pagMontarTurma'
+              element={
+                <ProtectedRoute allowedPermissions={['montarturma']}>
+                  <LayoutMontarTurma />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path='/pagDisciplina'
-            element={
-              <ProtectedRoute allowedPermissions={['disciplina']}>
-                <LayoutDisciplina />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path='/pagDisciplina'
+              element={
+                <ProtectedRoute allowedPermissions={['disciplina']}>
+                  <LayoutDisciplina />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path='/pagTurma'
-            element={
-              <ProtectedRoute allowedPermissions={['turma']}>
-                <LayoutTurma />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path='/pagTurma'
+              element={
+                <ProtectedRoute allowedPermissions={['turma']}>
+                  <LayoutTurma />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path='/configuracoes-de-seguranca'
-            element={
-              <ProtectedRoute allowedPermissions={['configuracoes']}>
-                <ConfiguracoesDeSeguranca />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/pagRegistroFrequencia'
-            element={
-              <ProtectedRoute allowedPermissions={['registrofrequencia']}>
-                <LayoutRegistroFrequencia />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/pagConsultarFrequencias'
-            element={
-              <ProtectedRoute allowedPermissions={['registrofrequencia']}>
-                <LayoutConsultaFrequencia />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path='/configuracoes-de-seguranca'
+              element={
+                <ProtectedRoute allowedPermissions={['configuracoes']}>
+                  <ConfiguracoesDeSeguranca />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/pagRegistroFrequencia'
+              element={
+                <ProtectedRoute allowedPermissions={['registrofrequencia']}>
+                  <LayoutRegistroFrequencia />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/pagConsultarFrequencias'
+              element={
+                <ProtectedRoute allowedPermissions={['registrofrequencia']}>
+                  <LayoutConsultaFrequencia />
+                </ProtectedRoute>
+              }
+            />
 
-                    <Route
-            path='/pagSubstituicoes'
-            element={
-              <ProtectedRoute allowedPermissions={['registrofrequencia']}>
-                <LayoutSubstituicoes/>
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path='/pagSubstituicoes'
+              element={
+                <ProtectedRoute allowedPermissions={['registrofrequencia']}>
+                  <LayoutSubstituicoes />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route path='/acesso-negado' element={<AcessoNegado />} />
-        </Routes>
+            <Route path='/acesso-negado' element={<AcessoNegado />} />
+          </Routes>
 
-        <ToastContainer position='bottom-right' autoClose={5000} />
-      </BrowserRouter>
+          <ToastContainer position='bottom-right' autoClose={5000} />
+        </BrowserRouter>
+      </LoadingProvider>
     </div>
   );
 }
